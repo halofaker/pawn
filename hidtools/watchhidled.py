31b44f67fbdc29769e7ec7ff8350c046177b2643
Swiftb0y <nikolaus.einhauser@web.de>
2017-09-19 21:02:59 +0200

    basic extension for watchhidled so triggers can be defined from bash.

--- hidtools/watchhidled.py
+++ hidtools/watchhidled.py
@@ -3,7 +3,7 @@
 
 #    This file is part of P4wnP1.
 #
-#    Copyright (c) 2017, Marcus Mengs. 
+#    Copyright (c) 2017, Marcus Mengs.
 #
 #    P4wnP1 is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ class WatchHIDLed:
 		self.state=[2, 2, 2] # [NUM, CAPS, SCROLL] 0 = LED off, 1 = LED on, 2 = not initialized
 		self.laststate=[2, 2, 2] # [NUM, CAPS, SCROLL] 0 = LED off, 1 = LED on, 2 = not initialized
 		self.last_change=[0, 0, 0] # [NUM, CAPS, SCROLL] milliseconds passed since last state change
-		self.count_change=[0, 0, 0] # [NUM, CAPS, SCROLL] count of state changes, gets reset after timeout 
+		self.count_change=[0, 0, 0] # [NUM, CAPS, SCROLL] count of state changes, gets reset after timeout
 		self.triggers=[]
 		self.timesaved = None
 
@@ -50,8 +50,8 @@ class WatchHIDLed:
 		# calculate delay in ms since last call (last LED state change)
 		if (self.timesaved == None):
 			self.timesaved = datetime.now()
-			delayms=0		
-		else:	
+			delayms=0
+		else:
 			lasttime = self.timesaved
 			self.timesaved = datetime.now()
 			delayms = int((self.timesaved - lasttime).total_seconds() * 1000)
@@ -122,7 +122,7 @@ class WatchHIDLed:
 					print "Trigger fired " + keyname + " pressed more than " + str(hitcount) + " times in short sequence, exitting with exit code " + str(exitcode)
 				sys.exit(exitcode)
 
-	# trigger: 
+	# trigger:
 	#	key		Which key should trigger (NUM LOCK, CAPS LOCK, SCROLL LOCK)
 	#	press_count	How often has the key to be pressed until the trigger fires
 	#			The delay betwenn keypresses has to be smaller than global timeout
@@ -152,7 +152,7 @@ class WatchHIDLed:
 			self.add_trigger("NUMLOCK", 5, 2)
 			self.add_trigger("SCROLLLOCK", 5, 3)
 
-		
+
 
 	def start_LED_monitoring(self):
 		with open("/dev/hidg0","rb") as f:
@@ -206,14 +206,14 @@ class WatchHIDLed:
 				out = '\x00\x00' + usbkey + '\x00\x00\x00\x00\x00' + '\x00\x00\x00\x00\x00\x00\x00\x00'
 				f.write(out)
 				f.flush()
-				
+
 				# check LED
 				data = f.read(1)
 				data = ord(data)
 				keystate = (data & ledmask) / ledmask
 				if keystate == targetstate:
 					return
-					
+
 
 
 	def send_receive_check(self, attempts = 5, testkey = "NUMLOCK", final_state = -1):
@@ -222,7 +222,7 @@ class WatchHIDLed:
 		if not testkey in KEYS:
 			return
 
-		# return exitcode 0 after attempts time changing the LED state for the key given by 
+		# return exitcode 0 after attempts time changing the LED state for the key given by
 		# testkey. Set key to off before exitting
 		self.add_trigger(testkey, attempts, 0, final_state)
 
@@ -245,18 +245,18 @@ class WatchHIDLed:
 					# send_key
 					out = '\x00\x00' + chr(KEYS[testkey]) + '\x00\x00\x00\x00\x00' + '\x00\x00\x00\x00\x00\x00\x00\x00'
 					f.write(out)
-					f.flush()					
+					f.flush()
 					# check LED
 					data = f.read(1)
 					self.statechange(ord(data))
-		
+
 
 	@staticmethod
 	def check_HID_availability():
 		t = WatchHIDLed()
 		# test with NUMLOCK, 5 LED changes needed to trigger, turn NUMLOCK off afterwards
 		t.send_receive_check(attempts = 5, testkey = "NUMLOCK", final_state = 0)
-		
+
 
 	@staticmethod
 	def createDefaultTrigger():
@@ -273,31 +273,38 @@ if __name__ == "__main__":
 	# exiting with different error codes on success is part of the core mechanism
 	# of this script, so it can't be used on exit
 	# we return with error code 255 if something went wrong, this has to be handled by the caller
-		
+
 	argc = len(sys.argv[1:])
 	if argc < 1:
 		sys.exit(255)
 
 	method = sys.argv[1]
-	
 
-	
+
+
 	if method == "check":
 		# check if HID keyboard is working by triggering LED
 		if argc == 1:
-			# no trigger detail given, use default 
+			# no trigger detail given, use default
 			# NUMLOCK is pressed until 5 LED state changes have been seen
 			WatchHIDLed.check_HID_availability()
 		else:
 			pass
 	elif method == "trigger":
+		t = WatchHIDLed()
 		# block execution till trigger occurs (trigger key, count of keypresses, exitcode)
 		if argc == 1:
 			# no trigger detail given, use default (5 presses of NUMLOCK, CAPSLOCK or SCROLLLOCK)
 			# exitcode 1 = CAPSLOCK, 2 = NUMLOCK, 3 = SCROLLLOCK pressed 5 times
-			WatchHIDLed.createDefaultTrigger()
+			t.createDefaultTrigger()
+		elif argc == 2:
+			t.add_trigger(sys.argv[1],sys.argv[2])
+			t.start_LED_monitoring()
+		elif argc == 3:
+			t.add_trigger(sys.argv[1],sys.argv[2],sys.argv[3])
+			t.start_LED_monitoring()
 		else:
 			# check of enough arguments for trigger definition
-			pass
+			print "invalid number of arguments!"
 	else:
 		sys.exit(255)
